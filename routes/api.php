1d00930fd29b2d41daa82d17e8e3c48f48dbc874
Giuseppe Impicciche <giuseppeimpicciche@Giuseppe.fritz.box>
2020-07-25 01:30:11 +0200

    Added JWT

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,17 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::resource('project', "ProjectController");
 Route::resource('task', "TaskController");
 Route::resource('client', "ClientController");
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
